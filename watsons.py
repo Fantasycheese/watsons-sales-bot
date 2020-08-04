b30dac52fa7eedd25ee27c612dc8a6cf23702910
Joshua <joshua@ampostech.com>
2020-08-04 10:48:04 +0800

    modify script

--- watsons.py
+++ watsons.py
@@ -7,7 +7,8 @@ j = [{
     'name': x['name'],
     'imageUrl': x['imgUrl'],
     'price': x['price'],
-    'url': x['pageUrl']
+    'url': x['detailUrl'],
+    'discount': x['discountText']
 } for x in df.iloc]
 ij = [json.dumps(i) + '\n' + json.dumps(j, ensure_ascii=False) for i, j in zip(i, j)]
 with open('result.txt', 'w', encoding='utf-8') as f:
